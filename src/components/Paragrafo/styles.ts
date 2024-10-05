c8b9e042dfc0b291a20eb9bf91c33ecc092bcce8
MarceloBenites1985 <marcelofelixbenite1985@gmail.com>
2024-10-05 00:36:56 -0300

    nao esta stylizando a cor do thema

--- src/components/Paragrafo/styles.ts
+++ src/components/Paragrafo/styles.ts
@@ -1,9 +1,9 @@
 import styled from 'styled-components'
-
 import type { Props } from '.'
 
 export const P = styled.p<Props>`
-  font-size: ${(props) => (props.fontSize ? `props.fontSize + 'px'` : '14px')};
-  color: ${(props) => (props.tipo === 'principal' ? '#282a35' : '#949494')};
+  font-size: ${(props) => (props.fontSize ? `${props.fontSize}px` : '14px')};
+  color: ${(props) =>
+    props.tipo === 'principal' ? props.theme.corPrincipal : '#949494'};
   line-height: 22px;
 `
